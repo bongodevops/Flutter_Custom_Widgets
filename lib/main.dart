09cc693df45a74a964a8fded2c97ac1a240d6725
bongodevops <bongodevops@gmail.com>
2025-03-18 00:29:58 +0600

    Text Details

--- lib/main.dart
+++ lib/main.dart
@@ -1,154 +1,139 @@
 import 'package:flutter/material.dart';
 
-main(){
-  runApp(const MyApp()); // Application
+void main() {
+  runApp(const MyApp());
 }
 
-
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return  MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.green,
-          iconTheme: IconThemeData(
-            color: Colors.white,
-          ),
-
-        ) ,
-
-        darkTheme: ThemeData(primarySwatch:Colors.amber),
-        color: Colors.blue,
-        debugShowCheckedModeBanner: false,
-        home:const HomeActivity()
+      title: 'Only Text Practice',
+      theme: ThemeData(
+        textTheme: TextTheme(
+          headlineLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          bodyLarge: TextStyle(fontSize: 16, color: Colors.black),
+        ),
+      ),
+      home: HomeScreen(),
     );
-
   }
 }
 
-
-class HomeActivity extends StatelessWidget{
-  const HomeActivity({super.key});
-
-
-  mySnackBar(message,context){
-    return ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(message))
-    );
-  }
-
-
-
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
+      //backgroundColor: const Color(0xFF10C419),
       appBar: AppBar(
-
-        title:  Text("Inventory App",style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
+        leading: const Icon(Icons.home, color: Colors.black54, size: 32,),
+        title: const Text('Home', style: TextStyle(
+            color: Colors.black
         ),),
-
-//// AppBar with Custom Background Gradient
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.blue, Colors.green],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-        ),
-
-        titleSpacing: 10,
-        toolbarHeight: 60,
-        toolbarOpacity: 1,
+        backgroundColor: Colors.yellow,
         elevation: 5,
-        //centerTitle: true,
-        // brightness: Brightness.dark,
-        // backgroundColor: Colors.amber,
-        backgroundColor: Colors.transparent,
-        //// AppBar with Custom Action Widget and Icon
-        actions: [
-          IconButton(onPressed: (){mySnackBar("I am comments",context);}, icon: const Icon(Icons.comment)),
-          IconButton(onPressed: (){mySnackBar("I am search",context);}, icon: const Icon(Icons.search)),
-          IconButton(onPressed: (){mySnackBar("I am settings",context);}, icon: const Icon(Icons.settings)),
-          IconButton(onPressed: (){mySnackBar("I am email",context);}, icon: const Icon(Icons.email))
+      ),
+      body: Column(
+        children: [
 
-        ],
+          Text(
 
+            'Hello,Bongo Devops.Flutter text details  Practice'.toUpperCase(),
+            //'Hello,Bongo Devops.Flutter text details  Practice'.toLowerCase(),
+            //textAlign: TextAlign.center,
+            //textAlign: TextAlign.start,
+            //textAlign: TextAlign.justify,
+            textAlign: TextAlign.center,
+            //textAlign: TextAlign.left,
+            //textAlign: TextAlign.right,
+            //textAlign: TextAlign.end,
 
-        //// PopupMenuButton
-        // actions: [
-        //   IconButton(
-        //     icon: Icon(Icons.search),
-        //     onPressed: () {
-        //       // Search action
-        //     },
-        //   ),
-        //   PopupMenuButton(
-        //     itemBuilder: (context) => [
-        //       PopupMenuItem(
-        //         child: Text('Settings'),
-        //         value: 'settings',
-        //       ),
-        //       PopupMenuItem(
-        //         child: Text('Logout'),
-        //         value: 'logout',
-        //       ),
-        //     ],
-        //     onSelected: (value) {
-        //       // Handle menu item selection
-        //     },
-        //   ),
-        // ],
-        leading: IconButton(onPressed: (){}, icon: Icon(Icons.menu)),
+            maxLines: 3,
+            /// ফ্লাটারে টেক্সট স্কেলিং ব্যবহার করে ইউজারের ডিভাইসের টেক্সট সাইজ প্রেফারেন্স অনুযায়ী টেক্সট সাইজ পরিবর্তন করা যায়।
+            textScaler: TextScaler.linear(1.5),
 
-        //// AppBar with Search
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+              backgroundColor: Colors.amber,
+              // backgroundColor: Colors.grey,
+              decoration: TextDecoration.underline,
+              //decoration: TextDecoration.lineThrough,
+              //decoration: TextDecoration.overline,
+              //decoration: TextDecoration.none,
+              wordSpacing: 4,
+              letterSpacing: 5,
+              overflow: TextOverflow.ellipsis,
+              //overflow: TextOverflow.fade,
+              //overflow: TextOverflow.clip,
+              //overflow: TextOverflow.visible,
 
-        // title: TextField(
-        //   decoration: InputDecoration(
-        //     hintText: 'Search...',
-        //     border: InputBorder.none,
-        //   ),
-        // ),
+            ),
 
-        ////একটি কাস্টম টাইটেল widget এবং আইকন সহ AppBar তৈরি করতে title প্রপার্টি ব্যবহার করুন।
-        // title: Row(
-        //   children: [
-        //     Icon(Icons.home),
-        //     SizedBox(width: 10),
-        //     Text('My App'),
-        //   ],
-        // ),
+          ),
 
-        //// AppBar with Custom Bottom Height
-        bottom: PreferredSize(
+          AnimatedDefaultTextStyle(
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 24,
+            ),
+            duration: Duration(seconds: 1),
+            child: Text('Hello, BongoDevops with Text animation'),
+          ),
 
-          preferredSize: Size.fromHeight(50.0),
-          child: Container(
-            color: Colors.blue,
-            height: 50.0,
-            child: Center(
-              child: Text('Custom Bottom Widget'),
+          /// টেক্সট শ্যাডো
+          Text(
+            'Hafizur Rahman Omar with shadow',
+            style: TextStyle(
+              fontSize: 24,
+              shadows: [
+                Shadow(
+                  color: Colors.black,
+                  offset: Offset(2, 2),
+                  blurRadius: 3,
+                ),
+              ],
+            ),
+
+          ),
+          ///  টেক্সট গ্রেডিয়েন্ট
+          ShaderMask(
+            shaderCallback: (bounds) {
+              return LinearGradient(
+                colors: [Colors.blue, Colors.red],
+              ).createShader(bounds);
+            },
+            child: Text(
+              'Hello, Hafizur Rahman with Text Gradian',
+              style: TextStyle(fontSize: 24, color: Colors.red),
             ),
           ),
-        ),
+          ///রিচ টেক্সট (RichText)
 
+          RichText(text: TextSpan(
+            text: 'Hello, Bong DevOps ',
+            style: TextStyle(color: Colors.black, fontSize: 20),
+            children: <TextSpan>[
+              TextSpan(
+                text: 'Flutter',
+                style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+              ),
+              TextSpan(
+                text: '!',
+                style: TextStyle(color: Colors.red, fontStyle: FontStyle.italic),
+              ),
+            ],
+          ),)
+        ],
       ),
 
-
-
-
     );
-
   }
-
-
-
 }
+
