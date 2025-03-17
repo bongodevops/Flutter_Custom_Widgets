04aebd848a6d0c777f566cfe6d867df401c49305
bongodevops <bongodevops@gmail.com>
2025-03-18 00:12:34 +0600

    first app

---
+++ android/app/src/main/kotlin/com/bongodevops/main/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bongodevops.main
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+
+/// converting a class into widgets so inherits the StatelessWidget
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 8,
+          title: Text('First App Main Code Flow'),
+        ),
+        body: Text('Hey I am Hafizur Rahman Omar. This is my new flutter app project'),
+      ),
+    );
+  }
+}
