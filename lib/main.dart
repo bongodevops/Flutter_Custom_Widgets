51df4fbc5b36e5aca6c02f6658b62e9627930587
bongodevops <bongodevops@gmail.com>
2025-03-18 00:22:34 +0600

    branch list

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,51 @@
-import 'package:flutter/material.dart';
+/// My repo has about 50+ branches.
 
-void main() {
-  runApp(const MyApp());
-}
-
-
-
-/// converting a class into widgets so inherits the StatelessWidget
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 8,
-          title: Text('First App Main Code Flow'),
-        ),
-        body: Text('Hey I am Hafizur Rahman Omar. This is my new flutter app project'),
-      ),
-    );
-  }
-}
+/// 1. main
+/// 2. AppBar
+/// 3. Floating action button
+/// 4. Bottom navigation bar
+/// 6. Navigation drawer
+/// 7. Row - Column
+/// 8. Container
+/// 9. Button
+/// 10. Simple form and alert dialog
+/// 11. Radio - Checkbox
+/// 12. TextFormField
+/// 13. Navigation / Routing screens
+/// 14. Circular Progress Indicator
+/// 15. Linear progress Indicator
+/// 16. Circle Avatar (set profile pic)
+/// 17. ListTile
+/// 18. ListView
+/// 19. MediaQuery
+/// 20. LayoutBuilder
+/// 21. AspectRatio
+/// 22. Orientation
+/// 23. Fractionally SizedBox
+/// 24. Expanded / Flexible
+/// 25. TabBar
+/// 26. Stateful Widget Lifecycle
+/// 27. Counter App
+/// 28. showModal Bottom sheet
+/// 29.TODO_app
+/// 30. Time picker
+/// 31. Date picker
+/// 32. Date Range picker
+/// 33. Rest Api intro
+/// 34. Rest Api GET
+/// 35. Rest Api POST
+/// 36. Task Manager App
+/// 37. GetX Routing
+/// 38. GetX State Management
+/// 39. Firebase
+/// 40. Firebase (realtime)
+/// 41. Firebase (storage)
+/// 42. Firebase (messaging)
+/// 43. Football Scorer made by Firebase real time database
+/// 44. Google Map
+/// 45. E commerce App
+/// 46. Quiz App
+/// 47. Food App
+///
+/// and If any update coming in my any branch or topic I will write here just simply that...
+/// for example "Updated Branch (branch name)"
