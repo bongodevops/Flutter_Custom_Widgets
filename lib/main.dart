2ee4a2c4db4bd98c8265f23db56d77536eaf9ce8
bongodevops <bongodevops@gmail.com>
2025-05-16 23:41:56 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -43,23 +43,93 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ),
           SizedBox(height: 20),
-          SizedBox(
-            height: 200,
-            width: 300,
-            child: Card(
-              color: Colors.orange,
-              elevation: 7.0,
-              child: ListTile(
-                title: Text(" This is My Card Title"),
-                subtitle: Text("Tis is My Subtitle"),
-                leading: Icon(Icons.add_call, color: Colors.white, size: 50.0),
-                trailing: Icon(
-                  Icons.arrow_forward_ios,
-                  color: Colors.white,
-                  size: 50.0,
-                ),
+          Column(
+            children: [
+              Stack(
+                children: [
+                  SizedBox(
+                    width: 300,
+                    height: 200,
+                    child: Card(
+                      color: Colors.orange,
+                      elevation: 7.0,
+                      child: ListTile(
+                        title: Text(" This is My Card Title"),
+                        subtitle: Text("Tis is My Subtitle"),
+                        leading: Icon(
+                          Icons.add_call,
+                          color: Colors.white,
+                          size: 50.0,
+                        ),
+                        trailing: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.white,
+                          size: 50.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 15,
+                    left: 20,
+                    child: Container(
+                      color: Colors.white,
+                      padding: EdgeInsets.all(5),
+                      child: Text(
+                        '4 .5',
+                        style: TextStyle(fontSize: 30, color: Colors.orange),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 15,
+                    right: 20,
+                    child: Container(
+                      padding: const EdgeInsets.all(10.0),
+                      color: Colors.white,
+                      child: Text(
+                        "Pabna",
+                        style: TextStyle(fontSize: 20, color: Colors.orange),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
+              SizedBox(height: 28),
+              Stack(
+                children: [
+                  Image.network(
+                    fit: BoxFit.cover,
+                    "https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg",
+                  ),
+
+                  Positioned(
+                    bottom: 15,
+                    left: 20,
+                    child: Container(
+                      color: Colors.white,
+                      padding: EdgeInsets.all(5),
+                      child: Text(
+                        '4 .5',
+                        style: TextStyle(fontSize: 30, color: Colors.orange),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 15,
+                    right: 20,
+                    child: Container(
+                      padding: const EdgeInsets.all(10.0),
+                      color: Colors.white,
+                      child: Text(
+                        "Pabna",
+                        style: TextStyle(fontSize: 20, color: Colors.orange),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ],
       ),
